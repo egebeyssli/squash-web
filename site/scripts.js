9cf12dd944c20543209e17dc608a39cdc5a6c68b
Egebey Sesli <ege@example.com>
2025-02-08 00:12:58 +0300

    İlk yükleme

---
+++ site/scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function() {
+    const program = document.getElementById("program");
+    program.innerHTML = "<p>Güncel maç programı eklenecek...</p>";
+});
